ddc2b04206e613ff0fdad82de2db9ecf4d0011fc
陳柏廷 <s0908070666@gmail.com>
2020-10-21 11:50:53 +0800

    練習 7 將 Route 的內容搬至 Controller 內

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/',['as'=>'home.index','uses'=>
+    'HomeController@index']);
+
 
 Route::get('/', function () {
     return view('welcome');
