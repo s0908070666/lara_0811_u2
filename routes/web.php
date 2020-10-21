ccea0ed37b9d37e9bc72db352fc46e68e99c7622
陳柏廷 <s0908070666@gmail.com>
2020-10-21 10:46:50 +0800

    練習二 設定 Route 接收參數

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,9 @@ Route::get('r2', function () {
     return view('welcome');
 });
 
+Route::get('hello/{name}',function($name){
+    return'Hello,'.$name;
+});
+
+Route::get('hello/{name?}',function($name='Everybody'){return'Hello,'.$name;
+});
