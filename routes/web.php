5074d5f1cc53bbab8e67698d49eb6cf92e38d13d
LAPTOP-NJIVUFM6\User <s0908070666@gmail.com>
2020-10-21 09:19:24 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('r1', function () {
+    return redirect('r2');
+});
+Route::get('r2', function () {
+    return view('welcome');
+});
+
